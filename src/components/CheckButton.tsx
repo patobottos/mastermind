c6bbe037b326c41da74762b8291338fb6bcc9004
patobottos <patobottos@gmail.com>
2024-06-03 12:57:26 +0200

    💄 Adding some top margin to CheckButton

--- src/components/CheckButton.tsx
+++ src/components/CheckButton.tsx
@@ -8,7 +8,7 @@ export default function CheckButton({ onClick }: CheckButtonProps) {
   return (
     <button
       type="button"
-      className="text-white bg-slate-700 hover:bg-slate-800 focus:outline-none focus:ring-4 focus:ring-slate-300 w-24 font-medium rounded-full text-sm px-2 py-2.5 text-center me-2 mb-2 dark:bg-slate-600 dark:hover:bg-slate-700 dark:focus:ring-slate-800 shadow-md"
+      className="text-white bg-slate-700 hover:bg-slate-800 focus:outline-none focus:ring-4 focus:ring-slate-300 w-24 font-medium rounded-full text-sm px-2 py-2.5 text-center m-2 dark:bg-slate-600 dark:hover:bg-slate-700 dark:focus:ring-slate-800 shadow-md"
       onClick={onClick}
       // disabled // REMOVE DISABLED ONLY AFTER COMPLETING FULL CODE GUESS!!
     >
