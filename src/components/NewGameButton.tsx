36ccccc51fbe1f19926f00c4ddc122e8397e58d3
Pato <patobottos@gmail.com>
2024-11-19 11:09:48 +0100

    🐛 Improving light/dark mode look for NewGame button

--- src/components/NewGameButton.tsx
+++ src/components/NewGameButton.tsx
@@ -8,7 +8,11 @@ export default function NewGameButton({ onClick }: NewGameButtonProps) {
   return (
     <button
       type="button"
-      className="text-slate-800 bg-slate-700 hover:bg-slate-800 focus:outline-none focus:ring-4 focus:ring-slate-300 w-24 font-medium rounded-full text-sm px-2 py-2.5 text-center m-2 dark:bg-sky-400 dark:hover:bg-sky-600 dark:hover:text-slate-100 dark:focus:ring-slate-800"
+      className="w-24 font-medium rounded-full text-sm px-2 py-2.5 text-center m-2 
+        font-sans text-slate-900 bg-slate-400 hover:bg-slate-300 focus:outline-none 
+        focus:ring-4 focus:ring-slate-400 dark:bg-sky-600 dark:text-slate-200 
+        dark:hover:bg-sky-500 dark:hover:text-white dark:focus:ring-sky-400 
+        transition-colors ease-in-out duration-300"
       onClick={onClick}
     >
       New Game
